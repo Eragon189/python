ef328197feea8ed0a66fc92abb8cfbc16cb1dee2
Eragon189 <144192904+Eragon189@users.noreply.github.com>
2024-11-21 22:36:20 +0000

    b

--- random_gen.py
+++ random_gen.py
@@ -11,14 +11,14 @@ upper_case_characters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", 
 lower_case_characters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
 special_characters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "[", "]", "|", "\\", "/", ":", ";", "\"", "\'", "<", ">", ",", ".", "?"]
 
-def char(a):
-    if a == 1:
+def char(b):
+    if b == 1:
         random_number1 = random.randint(0, len(special_characters) - 1)
         new_char = special_characters[random_number1]
-    elif a == 2:
+    elif b == 2:
         random_number1 = random.randint(0, len(lower_case_characters) - 1)
         new_char = lower_case_characters[random_number1]
-    elif a == 3:
+    elif b == 3:
         random_number1 = random.randint(0, len(upper_case_characters) - 1)
         new_char = upper_case_characters[random_number1]
 char(1)
