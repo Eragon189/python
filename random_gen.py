cf9100ae518908029337f116b99d44c7c48e6d59
Eragon189 <144192904+Eragon189@users.noreply.github.com>
2024-11-21 22:26:37 +0000

    done

--- random_gen.py
+++ random_gen.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 # Password settings
 password_length = int(input("How long of a Password do you whant:\n"))
 password = []
@@ -9,19 +11,25 @@ upper_case_characters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", 
 lower_case_characters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
 special_characters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "[", "]", "|", "\\", "/", ":", ";", "\"", "\'", "<", ">", ",", ".", "?"]
 
-# Generate password
-for i in range(password_length):
-    random_number = random.randint(1, 3)  # Randomly select category
-    if random_number == 1:
+def char(a):
+    if a == 1:
         random_number1 = random.randint(0, len(special_characters) - 1)
         new_char = special_characters[random_number1]
-    elif random_number == 2:
+    elif a == 2:
         random_number1 = random.randint(0, len(lower_case_characters) - 1)
         new_char = lower_case_characters[random_number1]
-    elif random_number == 3:
+    elif a == 3:
         random_number1 = random.randint(0, len(upper_case_characters) - 1)
         new_char = upper_case_characters[random_number1]
-    password.append(new_char)  # Corrected .push() to .append()
+char(1)
+char(2)
+char(3)
+# Generate password
+for i in range(password_length - 3):
+    
+    random_number = random.randint(1, 3)  # Randomly select category
+    char(random_number)
+    password.append(new_char)
 
 # Join password and display
 password_string = "".join(password)
